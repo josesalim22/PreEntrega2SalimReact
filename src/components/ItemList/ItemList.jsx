d43ef878375270a6d9617be30f7b178435a6b114
jose salim <josesalim22@yahoo.com>
2023-11-06 08:33:52 -0300

    estilos item list de flex a grid

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,7 +4,7 @@ import './ItemList.css';
 
 const ItemList = ({products}) => {
   return (
-    <div className='item-list-container'>
+    <div className='item-list'>
         {products.map(prod => <Item key={prod.id} {...prod} />)}
     </div>
   )
