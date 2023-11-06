1192a9cc6627575434969ada8241b67c0ecf5f39
jose salim <josesalim22@yahoo.com>
2023-11-06 07:54:18 -0300

    estilos cards item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Item.css';
 const Item = ({ id, name, img, price, stock }) => {
 
     return (
-        <article className='CardItem'>
-            <header className='Header'>
-                <h2 className='ItemHeader'>
+        <article className='card-item'>
+            <header className='card-header'>
+                <h2 className='item-header'>
                     {name}
                 </h2>
             </header>
             <picture>
-                <img src={img} alt={name} className='ItemImg' />
+                <img src={img} alt={name} className='item-img' />
             </picture>
             <section>
-                <p className='Info'>
+                <p className='info'>
                     Precio: ${price}
                 </p>
-                <p className='Info'>
+                <p className='info'>
                     Stock disponible: {stock}
                 </p>
             </section>
-            <footer className='ItemFooter'>
+            <footer className='item-footer'>
                 {/* <button className='Option'>Ver detalle</button> */}
-                <Link to={`/item/${id}`} className='Option'>Ver detalle</Link>
+                <Link to={`/item/${id}`} className='option'>Ver detalle</Link>
             </footer>
         </article>
 
