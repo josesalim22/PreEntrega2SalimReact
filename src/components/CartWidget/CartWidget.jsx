2b356573e13a2dbda1bdfb85aa9b694e3aa766ba
jose salim <josesalim22@yahoo.com>
2023-11-06 08:18:20 -0300

    estilos icono cart

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import cart from './assets/cart.png'
+import React from 'react';
+import cart from './assets/cart.png';
+import './CartWidget.css';
 
 const CartWidget = () => {
   return (
-    <div>
-        <img src={cart} alt="cart-widget" />
-        0
+    <div className='cart-widget'>
+        <img src={cart} alt="cart-widget" className='cart-icon' />
+        <span className='cart-count'>0</span>
 
     </div>
   )
