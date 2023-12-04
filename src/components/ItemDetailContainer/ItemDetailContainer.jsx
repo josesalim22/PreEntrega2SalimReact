7fbe1e22b28135442f44ab779a70133a15ea0ab0
jose salim <josesalim22@yahoo.com>
2023-12-04 10:58:09 -0300

    agregue mensaje cuando el producto no existe por id

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -6,23 +6,40 @@ import { getDoc, doc } from 'firebase/firestore';
 import { db } from '../../services/firebase/firebaseConfig';
 
 const ItemDetailContainer = () => {
-const [product, setProduct] = useState(null)
+  const [product, setProduct] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   const { itemId } = useParams()
 
-  useEffect(() => { 
+  useEffect(() => {
     const docRef = doc(db, 'products', itemId)
     getDoc(docRef)
       .then(response => {
-        const data = response.data()
-        const productAdapted = { id: response.id, ...data }
-        setProduct(productAdapted)
+        if (response.exists()) {
+
+          const data = response.data()
+          const productAdapted = { id: response.id, ...data }
+          setProduct(productAdapted)
+        } else {
+          console.error(`El producto con ID ${itemId} no existe.`)
+        }
+
       })
       .catch(error => {
         console.error(error)
       })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [itemId])
 
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (!product) {
+    return <h2 className='error-message'>El producto no existe.</h2>;
+  }
 
   return (
     <div className='ItemDetailContainer'>
